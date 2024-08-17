53b62db04fa43213594335a778815a93dcee5cf8
Winterson <u19cse1028@cit.ac.in>
2024-08-17 16:10:27 +0530

    add ui library, configure shadcn-ui, tailwind etc.

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,13 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const LoginPage = () => {
 	return (
-		<div>
-			<h1>Sign In</h1>
-			<input type="text" placeholder="Username" />
-			<input type="password" placeholder="Password" />
-			<button type="button">Log in</button>
-			<label htmlFor="remember-me">Remember me</label>
-			<input type="checkbox" id="remember-me" />
-			<a href="@">Forgot password</a>
+		<div className="h-screen w-full flex items-center justify-center">
+			<Card className="w-[350px] h-[350px] flex flex-col items-center justify-center">
+				<CardHeader>
+					<CardTitle className="text-center font-bold text-xl pb-5">
+						Sign In
+					</CardTitle>
+				</CardHeader>
+				<CardContent className="flex flex-col gap-6 w-full">
+					<Input type="text" placeholder="Username" />
+					<Input type="password" placeholder="Password" />
+					<section className="flex items-center justify-between ">
+						<section className="flex items-center gap-2">
+							<Checkbox id="remember-me" />
+							<Label htmlFor="remember-me">Remember me</Label>
+						</section>
+						<Button variant="link">
+							<a href="@">Forgot password</a>
+						</Button>
+					</section>
+					<Button variant="default">Log in</Button>
+				</CardContent>
+			</Card>
 		</div>
 	);
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest" />
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	test: {
 		environment: "jsdom",
 		setupFiles: "./setupTest.ts",
