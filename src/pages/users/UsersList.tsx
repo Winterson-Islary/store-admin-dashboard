e0bd2014403143b70c1bc9a0290e0c9afe2f33fd
Winterson <u19cse1028@cit.ac.in>
2024-09-29 17:47:11 +0530

    add: created user list component

---
+++ src/pages/users/UsersList.tsx
@@ -0,0 +1,147 @@
+import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { GetUsers } from "@/http/client";
+import type { TPageStateChange, UsersData } from "@/lib/types";
+import { useAuthStore } from "@/store";
+import { useQuery } from "@tanstack/react-query";
+import { List } from "lucide-react";
+import { Navigate } from "react-router-dom";
+import UserPagination from "./UserPagination";
+
+const UsersList = ({
+	pageState,
+	setPageState,
+}: {
+	pageState: TPageStateChange;
+	setPageState: React.Dispatch<React.SetStateAction<TPageStateChange>>;
+}) => {
+	console.log(pageState);
+	const { User } = useAuthStore();
+	if (User === null) {
+		return <Navigate to="/auth/login" replace />;
+	}
+	if (User.role !== "admin" && User.role !== "super") {
+		return <Navigate to="/" replace />;
+	}
+	const userQuery = useQuery({
+		queryKey: ["users", pageState],
+		queryFn: () => {
+			const temp = Object.keys(pageState).reduce(
+				(acc, key) => {
+					if (
+						pageState[key as keyof TPageStateChange] ===
+							undefined ||
+						pageState[key as keyof TPageStateChange] === ""
+					) {
+						return acc;
+					}
+					acc[key] = pageState[key as keyof TPageStateChange];
+					return acc;
+				},
+				{} as Record<string, string | boolean | number | undefined>,
+			);
+			const queryString = new URLSearchParams(
+				//! USE BETTER CONVERSION METHOD
+				temp as unknown as Record<string, string>,
+			).toString();
+			console.log(queryString);
+			return GetUsers(queryString);
+		},
+	});
+	if (userQuery.isLoading) {
+		return (
+			<div className="h-screen flex  justify-center">
+				<p className="text-2xl mt-20">Loading...</p>
+			</div>
+		);
+	}
+
+	console.log(userQuery.data);
+
+	const usersData: UsersData[] = !userQuery.isError
+		? userQuery.data.users
+		: [];
+	return (
+		<main>
+			<div className="px-5">
+				<section className=" 2xl:w-[70%] mx-auto">
+					<header className="flex justify-between">
+						<CardHeader className="px-0">
+							<CardTitle className="flex gap-2 items-center uppercase">
+								{" "}
+								<List height={20} width={20} />
+								<span>Users List</span>
+							</CardTitle>
+
+							<CardDescription>
+								List of registered users
+							</CardDescription>
+						</CardHeader>
+						<section>
+							{userQuery.isError && (
+								<div>{userQuery.error.message}</div>
+							)}
+						</section>
+					</header>
+
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>Username</TableHead>
+
+								<TableHead>Status</TableHead>
+
+								<TableHead>Role</TableHead>
+
+								<TableHead>Email</TableHead>
+
+								<TableHead>Registration Date</TableHead>
+							</TableRow>
+						</TableHeader>
+
+						<TableBody>
+							{usersData.map((user) => (
+								<TableRow key={user.id}>
+									<TableCell>{user.name}</TableCell>
+
+									<TableCell>
+										{user.isActive ? "ACTIVE" : "INACTIVE"}
+									</TableCell>
+
+									<TableCell>{user.role}</TableCell>
+
+									<TableCell>{user.email}</TableCell>
+
+									<TableCell>
+										{
+											user.createdAt
+
+												.toString()
+
+												.split("T")[0]
+										}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</section>
+			</div>{" "}
+			<section className="mt-5">
+				<UserPagination
+					usersData={userQuery.data}
+					setPageState={setPageState}
+				/>
+			</section>
+		</main>
+	);
+};
+
+export default UsersList;
